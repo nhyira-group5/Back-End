71d57c5d357beab8317735f69c2ddcf2a1fa0404
Will Gustavo Dantas <71149012+WillDantasJPG@users.noreply.github.com>
2024-10-03 13:37:39 -0300

    feature:main.tf

--- src/infraNhyira/moduleEc2/main.tf
+++ src/infraNhyira/moduleEc2/main.tf
@@ -28,36 +28,47 @@ set -x
 # Atualiza pacotes e instala Java
 sudo apt-get update -y
 sudo apt-get install -y default-jdk
-sudo apt-get install -y git # Adicionando a instalação do git
+sudo apt-get install -y git
 
-# Instala Docker
-sudo apt-get install -y docker.io
+# Atualiza o repositório novamente
+sudo apt-get update -y
+
+# Instala o Docker
+sudo apt-get install -y docker-ce
+
+# Verifica se o Docker está instalado corretamente
 sudo systemctl start docker
 sudo systemctl enable docker
 
 # Instala Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compos
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Cria o diretório se não existir
+mkdir -p /home/ubuntu/AWS
 
 # Clonar ou atualizar o repositório
 cd /home/ubuntu/AWS || {
-  git clone https://github.com/nhyira-group5/Back-End.git /home/ubuntu/websocket
+  git clone https://github.com/nhyira-group5/Back-End.git /home/ubuntu/AWS
 }
 
 cd /home/ubuntu/AWS
 git pull origin main  # Atualiza o repositório
 
-  # Navega até o diretório do projeto
-  cd /home/ubuntu/AWS
+# Navega até o diretório do projeto
+cd /home/ubuntu/AWS
 
-  # Constrói a imagem Docker usando o Dockerfile
-  sudo docker build -t nhyira-api .
+# Constrói a imagem Docker usando o Dockerfile
+sudo docker build -t nhyira-api .
 
-  # Executa o Docker Compose para iniciar os serviços
-  sudo docker-compose up -d
-  EOF
+# Executa o Docker Compose para iniciar os serviços
+ sudo docker-compose -f /home/ubuntu/AWS/docker-compose.yml up -d
+
+   EOF
   )
 }
+
+
 
 resource "aws_instance" "private_ec2_backend_2" {
   ami               = var.ami
@@ -75,7 +86,6 @@ resource "aws_instance" "private_ec2_backend_2" {
   tags = {
     Name = "private-ec2-02"
   }
-
 user_data = base64encode(<<-EOF
 #!/bin/bash
 exec > /var/log/user_data.log 2>&1
@@ -84,34 +94,42 @@ set -x
 # Atualiza pacotes e instala Java
 sudo apt-get update -y
 sudo apt-get install -y default-jdk
-sudo apt-get install -y git # Adicionando a instalação do git
+sudo apt-get install -y git
 
-# Instala Docker
-sudo apt-get install -y docker.io
+# Atualiza o repositório novamente
+sudo apt-get update -y
+
+# Instala o Docker
+sudo apt-get install -y docker-ce
+
+# Verifica se o Docker está instalado corretamente
 sudo systemctl start docker
 sudo systemctl enable docker
 
 # Instala Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compos
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Cria o diretório se não existir
+mkdir -p /home/ubuntu/AWS
 
 # Clonar ou atualizar o repositório
 cd /home/ubuntu/AWS || {
-  git clone https://github.com/nhyira-group5/Back-End.git /home/ubuntu/websocket
+  git clone https://github.com/nhyira-group5/Back-End.git /home/ubuntu/AWS
 }
 
 cd /home/ubuntu/AWS
 git pull origin main  # Atualiza o repositório
 
-  # Navega até o diretório do projeto
-  cd /home/ubuntu/AWS
+# Navega até o diretório do projeto
+cd /home/ubuntu/AWS
 
-  # Constrói a imagem Docker usando o Dockerfile
-  sudo docker build -t nhyira-api .
+# Constrói a imagem Docker usando o Dockerfile
+sudo docker build -t nhyira-api .
 
-  # Executa o Docker Compose para iniciar os serviços
-  sudo docker-compose up -d
-  EOF
+# Executa o Docker Compose para iniciar os serviços
+ sudo docker-compose -f /home/ubuntu/AWS/docker-compose.yml up -d
+   EOF
   )
 }
 
