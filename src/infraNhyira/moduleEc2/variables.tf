cd55d76b83e43652741c5b238dcd00df38bf3877
Will Gustavo Dantas <71149012+WillDantasJPG@users.noreply.github.com>
2024-11-03 22:45:12 -0300

    Update variables.tf

--- src/infraNhyira/moduleEc2/variables.tf
+++ src/infraNhyira/moduleEc2/variables.tf
@@ -13,10 +13,9 @@ variable "key_pair_name" {
 variable "ami" {
   description = "AMI ID"
   type        = string
-  default     = "ami-048c8cb78e7408897"  # O ID da AMI 
+  default     = "ami-0167aed34e622a91c"  # O ID da AMI 
 
 }
-
 
 variable "inst_type" {
   description = "Instance Type"
@@ -27,13 +26,13 @@ variable "inst_type" {
 variable "subnet_id" {
   description = "Subnet ID"
   type        = string
-  default     = "subnet-09424067824895155"
+  default     = "subnet-0e2701ab99c4d1b94"
 }
 
 variable "sg_id" {
   description = "Security Group ID"
   type        = string
-  default     = "sg-08a6c790338e94c72"
+  default     = "sg-05ad6fffec857ca5d"
 }
 
 variable "dockerhub_username" {
